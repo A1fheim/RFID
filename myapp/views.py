042321143e40e5596eba1244d3dc5da9ec6418ca
Alfheim <19prof.fessional99@gmail.com>
2024-10-05 19:49:38 +0300

    Удалён столбец 'Количество людей', реализован отдельный счётчик людей 5

--- myapp/views.py
+++ myapp/views.py
@@ -12,7 +12,6 @@ from django.db.models import F
 from .models import PeopleCounter  # Импортируем модель из models.py
 
 
-
 # Класс для обработки POST-запросов с использованием Django REST Framework
 class TagEntryView(APIView):
     def post(self, request):
@@ -35,7 +34,14 @@ class TagEntryView(APIView):
         new_entry = TagEntry.objects.create(tag_id=tag_id)
 
         # Увеличиваем количество людей, если метка новая
-        PeopleCounter.objects.all().update(count=F('count') + 1)
+        people_counter = PeopleCounter.objects.first()
+        if not people_counter:
+            # Если запись отсутствует, создаём её с начальным значением 0
+            people_counter = PeopleCounter.objects.create(count=0)
+        
+        # Увеличиваем счётчик
+        people_counter.count = F('count') + 1
+        people_counter.save()
 
         # Получаем текущее количество людей
         people_count = PeopleCounter.objects.first().count
@@ -44,27 +50,22 @@ class TagEntryView(APIView):
         return Response({'entry': serializer.data, 'people_count': people_count}, status=status.HTTP_201_CREATED)
 
 
-# Функция для отображения таблицы с данными из модели TagEntry
-def table_view(request):
-    entries = TagEntry.objects.all()  # Получаем все записи из модели TagEntry
-    return render(request, 'table.html', {'entries': entries})
-
-
-# Функция для отображения главной страницы
-def home(request):
-    return render(request, 'home.html')
-
-
 # Функция для получения последней записи из модели
 def get_latest_entry(request):
     try:
         latest_entry = TagEntry.objects.latest('id')
+        # Проверяем, существует ли запись в PeopleCounter
+        people_counter = PeopleCounter.objects.first()
+        if not people_counter:
+            # Если запись отсутствует, создаём её с начальным значением 0
+            people_counter = PeopleCounter.objects.create(count=0)
+        
         data = {
             'tag_id': latest_entry.tag_id,
             'entry_time': latest_entry.entry_time.strftime('%d/%m/%Y %H:%M:%S'),
             'exit_time': latest_entry.exit_time.strftime('%d/%m/%Y %H:%M:%S') if latest_entry.exit_time else '',
             'total_time': str(latest_entry.total_time) if latest_entry.total_time else '',
-            'people_count': PeopleCounter.objects.first().count  # Получаем текущее количество людей
+            'people_count': people_counter.count  # Получаем текущее количество людей
         }
         return JsonResponse(data)
     except TagEntry.DoesNotExist:
